3c7332d16f10bbc9681362bee8e372e8117ddae3
Cihan Sungur <sungur1988@gmail.com>
2022-09-11 19:23:09 +0300

    AppDbContextCreated

---
+++ src/Kodlama.io/Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Contexts
+{
+    public  class AppDbContext : DbContext
+    {
+        public DbSet<ProgrammingLanguage> ProgrammingLanguages { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+    }
+}
