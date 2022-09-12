4b72f9f5161bed1a0d9e48068236d9c495e1a8c5
Cihan Sungur <sungur1988@gmail.com>
2022-09-12 22:21:50 +0300

    ProgrammingLanguageBusinessRulesCreated

--- src/Kodlama.io/Application/Features/ProgrammingLanguages/Commands/CreateProgrammingLanguage/CreateProgrammingLanguageCommandHandler.cs
+++ src/Kodlama.io/Application/Features/ProgrammingLanguages/Commands/CreateProgrammingLanguage/CreateProgrammingLanguageCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Features.ProgrammingLanguages.Dtos;
+using Application.Features.ProgrammingLanguages.Rules;
 using Application.Services.Repositories;
 using AutoMapper;
 using Domain.Entities;
@@ -15,15 +16,19 @@ namespace Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLa
     {
         private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
         private readonly IMapper _mapper;
+        private readonly ProgrammingLanguageBusinessRules _programmingLanguageBusinessRules;
 
-        public CreateProgrammingLanguageCommandHandler(IProgrammingLanguageRepository programmingLanguageRepository, IMapper mapper)
+        public CreateProgrammingLanguageCommandHandler(IProgrammingLanguageRepository programmingLanguageRepository, IMapper mapper, ProgrammingLanguageBusinessRules programmingLanguageBusinessRules)
         {
             _programmingLanguageRepository = programmingLanguageRepository;
             _mapper = mapper;
+            _programmingLanguageBusinessRules = programmingLanguageBusinessRules;
         }
 
         public async Task<CreatedProgrammingLanguageDto> Handle(CreateProgrammingLanguageCommand request, CancellationToken cancellationToken)
         {
+            await _programmingLanguageBusinessRules.ProgrammingLanguageCannotBeDuplicatedWhenInsertedOrUpdated(request.Name);
+
             var entityToAdd = _mapper.Map<ProgrammingLanguage>(request);
             var addedEntity = await _programmingLanguageRepository.AddAsync(entityToAdd);
             var mappedEntity = _mapper.Map<CreatedProgrammingLanguageDto>(addedEntity);
---
+++ src/Kodlama.io/Application/Features/ProgrammingLanguages/Rules/ProgrammingLanguageBusinessRules.cs
@@ -0,0 +1,26 @@
+﻿using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.Rules
+{
+    public class ProgrammingLanguageBusinessRules
+    {
+        private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+
+        public ProgrammingLanguageBusinessRules(IProgrammingLanguageRepository programmingLanguageRepository)
+        {
+            _programmingLanguageRepository = programmingLanguageRepository;
+        }
+
+        public async Task ProgrammingLanguageCannotBeDuplicatedWhenInsertedOrUpdated(string name)
+        {
+            var entities = await _programmingLanguageRepository.GetListAsync(x => x.Name == name);
+            if (entities.Items.Any()) throw new BusinessException($"Programming language name {name} exists.");
+        }
+    }
+}
