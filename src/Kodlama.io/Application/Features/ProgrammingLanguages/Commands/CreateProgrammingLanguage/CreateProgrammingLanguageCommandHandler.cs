5f5a8928306d7901f761ae30c53c6823befa16fa
Cihan Sungur <sungur1988@gmail.com>
2022-09-12 22:15:29 +0300

    CreateProgrammingLanguageFeatureAdded

---
+++ src/Kodlama.io/Application/Features/ProgrammingLanguages/Commands/CreateProgrammingLanguage/CreateProgrammingLanguageCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Application.Features.ProgrammingLanguages.Dtos;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLanguage
+{
+    public class CreateProgrammingLanguageCommandHandler : IRequestHandler<CreateProgrammingLanguageCommand, CreatedProgrammingLanguageDto>
+    {
+        private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+        private readonly IMapper _mapper;
+
+        public CreateProgrammingLanguageCommandHandler(IProgrammingLanguageRepository programmingLanguageRepository, IMapper mapper)
+        {
+            _programmingLanguageRepository = programmingLanguageRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CreatedProgrammingLanguageDto> Handle(CreateProgrammingLanguageCommand request, CancellationToken cancellationToken)
+        {
+            var entityToAdd = _mapper.Map<ProgrammingLanguage>(request);
+            var addedEntity = await _programmingLanguageRepository.AddAsync(entityToAdd);
+            var mappedEntity = _mapper.Map<CreatedProgrammingLanguageDto>(addedEntity);
+
+            return mappedEntity;
+        }
+    }
+}
