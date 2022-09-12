bc6854071ba763f740ba56884fe1650d05c8f1fe
Cihan Sungur <sungur1988@gmail.com>
2022-09-12 22:25:11 +0300

    ServiceRegistrationsAdded

---
+++ src/Kodlama.io/Application/ServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Application.Features.ProgrammingLanguages.Rules;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServiceRegistration(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddScoped(typeof(ProgrammingLanguageBusinessRules));
+
+            return services;
+        }
+    }
+}
