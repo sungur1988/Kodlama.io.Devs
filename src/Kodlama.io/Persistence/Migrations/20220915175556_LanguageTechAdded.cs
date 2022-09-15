e05163b1c72870a8e2ee19c4807d160aa02aec63
Cihan Sungur <sungur1988@gmail.com>
2022-09-15 21:01:33 +0300

    MigrationCreated

---
+++ src/Kodlama.io/Persistence/Migrations/20220915175556_LanguageTechAdded.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class LanguageTechAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LanguageTechs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProgrammingLanguageId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LanguageTechs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LanguageTechs_ProgrammingLanguages_ProgrammingLanguageId",
+                        column: x => x.ProgrammingLanguageId,
+                        principalTable: "ProgrammingLanguages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "LanguageTechs",
+                columns: new[] { "Id", "Name", "ProgrammingLanguageId" },
+                values: new object[,]
+                {
+                    { 1, "WPF", 1 },
+                    { 2, "ASP.NET", 1 },
+                    { 3, "Spring", 2 },
+                    { 4, "JSP", 2 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LanguageTechs_ProgrammingLanguageId",
+                table: "LanguageTechs",
+                column: "ProgrammingLanguageId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LanguageTechs");
+        }
+    }
+}
