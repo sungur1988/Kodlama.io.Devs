78b6ce74c84b5ac69c6d85e505c8a7c515991abd
Cihan Sungur <sungur1988@gmail.com>
2022-09-27 01:08:11 +0300

    ConfigurationsAddedForSecurity

---
+++ src/Kodlama.io/Persistence/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Core.Security.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Configurations
+{
+    public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder.HasKey(x=> x.Id);
+            builder.HasOne(x => x.User);
+        }
+    }
+}
---
+++ src/Kodlama.io/Persistence/Configurations/UserOperationClaimConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Core.Security.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Configurations
+{
+    public class UserOperationClaimConfiguration : IEntityTypeConfiguration<UserOperationClaim>
+    {
+        public void Configure(EntityTypeBuilder<UserOperationClaim> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasOne(x => x.User);
+            builder.HasOne(x => x.OperationClaim);
+        }
+    }
+}
