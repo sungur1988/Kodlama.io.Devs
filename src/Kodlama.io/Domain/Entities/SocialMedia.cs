dddc728d94eb01d31796ead51fffc654cf618c5d
Cihan Sungur <sungur1988@gmail.com>
2022-09-30 17:31:45 +0300

    SocialMediaCreated

---
+++ src/Kodlama.io/Domain/Entities/SocialMedia.cs
@@ -0,0 +1,29 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class SocialMedia :Entity
+    {
+        public string Name { get; set; }
+        public string UrlAddress { get; set; }
+        public int AppUserId { get; set; }
+        public virtual AppUser AppUser { get; set; }
+
+        public SocialMedia()
+        {
+
+        }
+        public SocialMedia(int id,string name,string urlAddress,int appUserId):this()
+        {
+            Id = id;
+            Name = name;
+            UrlAddress = urlAddress;
+            AppUserId = appUserId;
+        }
+    }
+}
