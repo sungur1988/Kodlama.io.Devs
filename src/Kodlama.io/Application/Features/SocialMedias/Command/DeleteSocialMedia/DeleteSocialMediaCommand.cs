f4fd1fc61939491508f2920cd77a51131e723e33
Cihan Sungur <sungur1988@gmail.com>
2022-10-08 14:49:45 +0300

    DeleteSocialMediaFeatureAdded

---
+++ src/Kodlama.io/Application/Features/SocialMedias/Command/DeleteSocialMedia/DeleteSocialMediaCommand.cs
@@ -0,0 +1,12 @@
+﻿using Application.Features.SocialMedias.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.SocialMedias.Command.DeleteSocialMedia
+{
+    public record DeleteSocialMediaCommand(int Id,string Name,string UrlAddress) : IRequest<DeletedSocialMediaDto>;
+}
